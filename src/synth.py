4cb1190b9c5eb3ca1841553849401b95c22b5740
Stasinos Konstantopoulos <konstant@iit.demokritos.gr>
2025-10-22 11:55:03 +0300

    Many polys

--- src/synth.py
+++ src/synth.py
@@ -8,70 +8,81 @@ import matplotlib.pyplot as plt
 
 
 random.seed( 42 )
-r = numpy.zeros( 12 )
-a = numpy.zeros( 12 )
 shapes = []
+positives = range(5)
 
-# three random lengths from 0,0 make a triangle
-r[0] = random.random() + 0.5
-r[4] = random.random() + 0.5
-r[8] = random.random() + 0.5
-a = [i*numpy.pi/6 for i in range(12)]
+# Each iteration makes 10 polys, from 3 to 12
+for batch in range(10):
 
-coords = [None]*12
-for i in [0,4,8]:
-    coords[i] = (r[i]*numpy.cos(a[i]), r[i]*numpy.sin(a[i]))
+    r = numpy.zeros( 12 )
+    a = numpy.zeros( 12 )
 
-sh = shapely.geometry.Polygon( [c for c in coords if c != None] )
-n = 3
-shapes.append( [sh,n] )
+    # three random lengths from 0,0 make a triangle
+    r[0] = random.random() + 0.5
+    r[4] = random.random() + 0.5
+    r[8] = random.random() + 0.5
+    a = [i*numpy.pi/6 for i in range(12)]
 
-plt.plot( *sh.exterior.xy )
-plt.axis( "off" )
-plt.savefig( f"{n}.png" )
-plt.close()
+    coords = [None]*12
+    for i in [0,4,8]:
+        coords[i] = (r[i]*numpy.cos(a[i]), r[i]*numpy.sin(a[i]))
 
-# All indexes to the right of those already used
-# This will make 4,5,6
-for i in numpy.nonzero(r)[0] + 1:
-    # find the distance from 0,0 to sh at angle theta:
-    # get a long line from 0,0 outwards
-    l = shapely.geometry.LineString( [(0,0), (1*numpy.cos(a[i]), 1*numpy.sin(a[i]))] )
-    cross = sh.intersection( l )
-    # This is the part of l that is within the poly.
-    # So the second coord is the intersection point, find the distance
-    d = numpy.sqrt( cross.xy[0][1]**2 + cross.xy[1][1]**2 )
-    # perturb to move the new vertex away from the existing edge
-    r[i] = d + random.random() + 0.3
-    coords[i] = (r[i]*numpy.cos(a[i]), r[i]*numpy.sin(a[i]))
-    n += 1
     sh = shapely.geometry.Polygon( [c for c in coords if c != None] )
-    shapes.append( [sh,n] )
+    n = 3
+    fname = f"{batch:02d}_{n:02d}.png"
+    shapes.append( [fname,sh,n,n in positives] )
 
     plt.plot( *sh.exterior.xy )
     plt.axis( "off" )
-    plt.savefig( f"{n}.png" )
+    plt.savefig( fname )
     plt.close()
 
-# Repeat to make 7, 8, 9, 10, 11, 12
-for i in numpy.nonzero(r)[0] + 1:
-    # find the distance from 0,0 to sh at angle theta:
-    # get a long line from 0,0 outwards
-    l = shapely.geometry.LineString( [(0,0), (1*numpy.cos(a[i]), 1*numpy.sin(a[i]))] )
-    cross = sh.intersection( l )
-    # This is the part of l that is within the poly.
-    # So the second coord is the intersection point, find the distance
-    d = numpy.sqrt( cross.xy[0][1]**2 + cross.xy[1][1]**2 )
-    # perturb to move the new vertex away from the existing edge
-    r[i] = d + random.random() + 0.3
-    coords[i] = (r[i]*numpy.cos(a[i]), r[i]*numpy.sin(a[i]))
-    n += 1
-    sh = shapely.geometry.Polygon( [c for c in coords if c != None] )
-    shapes.append( [sh,n] )
+    # All indexes to the right of those already used
+    # This will make 4,5,6
+    for i in numpy.nonzero(r)[0] + 1:
+        # find the distance from 0,0 to sh at angle theta:
+        # get a long line from 0,0 outwards
+        l = shapely.geometry.LineString( [(0,0), (1*numpy.cos(a[i]), 1*numpy.sin(a[i]))] )
+        cross = sh.intersection( l )
+        # This is the part of l that is within the poly.
+        # So the second coord is the intersection point, find the distance
+        d = numpy.sqrt( cross.xy[0][1]**2 + cross.xy[1][1]**2 )
+        # perturb to move the new vertex away from the existing edge
+        r[i] = d + random.random() + 0.3
+        coords[i] = (r[i]*numpy.cos(a[i]), r[i]*numpy.sin(a[i]))
+        n += 1
+        sh = shapely.geometry.Polygon( [c for c in coords if c != None] )
+        fname = f"{batch:02d}_{n:02d}.png"
+        shapes.append( [fname,sh,n,n in positives] )
 
-    plt.plot( *sh.exterior.xy )
-    plt.axis( "off" )
-    plt.savefig( f"{n}.png" )
-    plt.close()
+        plt.plot( *sh.exterior.xy )
+        plt.axis( "off" )
+        plt.savefig( fname )
+        plt.close()
 
-print( shapes )
+    # Repeat to make 7, 8, 9, 10, 11, 12
+    for i in numpy.nonzero(r)[0] + 1:
+        # find the distance from 0,0 to sh at angle theta:
+        # get a long line from 0,0 outwards
+        l = shapely.geometry.LineString( [(0,0), (1*numpy.cos(a[i]), 1*numpy.sin(a[i]))] )
+        cross = sh.intersection( l )
+        # This is the part of l that is within the poly.
+        # So the second coord is the intersection point, find the distance
+        d = numpy.sqrt( cross.xy[0][1]**2 + cross.xy[1][1]**2 )
+        # perturb to move the new vertex away from the existing edge
+        r[i] = d + random.random() + 0.3
+        coords[i] = (r[i]*numpy.cos(a[i]), r[i]*numpy.sin(a[i]))
+        n += 1
+        sh = shapely.geometry.Polygon( [c for c in coords if c != None] )
+        fname = f"{batch:02d}_{n:02d}.png"
+        shapes.append( [fname,sh,n,n in positives] )
+
+        plt.plot( *sh.exterior.xy )
+        plt.axis( "off" )
+        plt.savefig( fname )
+        plt.close()
+
+labels = pandas.DataFrame( shapes, columns=["fname","shape","n","label"] )
+labels.to_csv( "labels.csv" )
+
+
